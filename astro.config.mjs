d8006db4e4b47b7ddb0d9937f5346c72dce7dbde
ZeruasUtp <irvinvazquez422@gmail.com>
2025-10-22 14:12:44 -0600

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://suarezIrvin.github.io',
-  base: '/suarezIrvin.github.io'
+  base: '/suarezIrvin.github.io',
 })
